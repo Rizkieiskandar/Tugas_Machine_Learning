bab9d67ea7e955f030a67edf7d9afa317ffcf643
Rizkie Pratama Putra Iskandar <rizkie@Rizkies-MacBook-Air.local>
2024-05-17 20:03:35 +0700

    first commit

---
+++ Main.py
@@ -0,0 +1,11 @@
+#jawaban soal 1
+nama = "Rizkie Pratama Putra Iskandar"
+nomor_induk_mahasiswa = "20220801425"
+program_studi = "Teknis Informatika"
+fakultas = "Ilmu Komputer"
+
+print("DATA DIRI")
+print("Nama : ", nama)
+print("NIM : ", nomor_induk_mahasiswa)
+print("Program Studi : ", program_studi)
+print("Fakultas :", fakultas)
